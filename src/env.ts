d9a3cd42110fa71df3ef28d46eea38086493cfcc
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-06-12 12:41:26 -0300

    build: jwt authentication setup

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
   DB_URL: z.string().url().min(1),
   API_BASE_URL: z.string().url().min(1),
   AUTH_REDIRECT_URL: z.string().url().min(1),
+  JWT_SECRET_KEY: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
-import { Elysia } from 'elysia'
+import cookie from '@elysiajs/cookie'
+import jwt from '@elysiajs/jwt'
+import { Elysia, t } from 'elysia'
 
+import { env } from '../env'
 import { registerRestaurant } from './route/register-restaurant'
 import { sendAuthLink } from './route/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
