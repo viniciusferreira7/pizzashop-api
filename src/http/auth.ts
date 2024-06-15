f4de06cc6752ce10ac1b60cac1238f3ad99c00f8
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-06-15 09:20:10 -0300

    feat: creates the authentication route by link

---
+++ src/http/auth.ts
@@ -0,0 +1,14 @@
+import jwt from '@elysiajs/jwt'
+import { Elysia, t } from 'elysia'
+
+import { env } from '../env'
+
+export const auth = new Elysia().use(
+  jwt({
+    secret: env.JWT_SECRET_KEY,
+    schema: t.Object({
+      sub: t.String(),
+      restaurantId: t.Optional(t.String()),
+    }),
+  }),
+)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,24 +1,13 @@
-import cookie from '@elysiajs/cookie'
-import jwt from '@elysiajs/jwt'
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 
-import { env } from '../env'
+import { authenticateFromLink } from './route/authenticate-from-link'
 import { registerRestaurant } from './route/register-restaurant'
 import { sendAuthLink } from './route/send-auth-link'
 
 const app = new Elysia()
-  .use(
-    jwt({
-      secret: env.JWT_SECRET_KEY,
-      schema: t.Object({
-        sub: t.String(),
-        restaurantId: t.Optional(t.String()),
-      }),
-    }),
-  )
-  .use(cookie())
   .use(registerRestaurant)
   .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
